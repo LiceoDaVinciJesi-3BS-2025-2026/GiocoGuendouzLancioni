1dc0b6ca25738b4a866a3fccafbf131a440280bd
elenalancioni10 <elena.lancioni10@gmail.com>
2026-03-23 15:21:30 +0100

    resources

---
+++ src/missionone/resources.py
@@ -0,0 +1,15 @@
+# resources - presenta le funzioni per caricare materiali(immagini e suoni) nel gioco
+
+# Authors_
+# Elena Lancioni
+# Aziz Guendouz
+
+from importlib.resources import files
+from pathlib import Path
+
+# eventualmente aggiungere tutte le funzioni relative alle cartelle che avete creato in src.
+def get_sound(filename: str) -> Path:
+    return files(__package__) / "sounds" / filename
+
+def get_image(filename: str) -> Path:
+    return files(__package__) / "images" / filename
